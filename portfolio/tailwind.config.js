08eeb3037191df490206b9e57fb60be8480c38f1
unknown <wjlee2821@gmail.com>
2024-11-16 21:14:19 +0800

    Initial commit

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all React files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
